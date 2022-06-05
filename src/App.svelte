e872719215a726ec467bb18a925393e618ebb267
rasjonell <info.gurgen@gmail.com>
2022-06-06 02:50:46 +0400

    Separate Typer component into it's own file

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,29 @@
 <script lang="ts">
-  import { tick } from "svelte";
-
+  import Typer from "./components/typer.svelte";
   import Keyboard from "./components/keyboard.svelte";
-
-  let value = "";
-  let pressedKey: string = null;
-
-  async function handleChange({ key }: KeyboardEvent): Promise<void> {
-    pressedKey = key;
-    await tick();
-    pressedKey = null;
-  }
 </script>
 
 <main>
-  <input
-    bind:value
-    type="text"
-    on:keydown={handleChange}
-    placeholder="Type some shit..."
-  />
-  <Keyboard activeKey={pressedKey} />
+  <Typer />
+  <Keyboard />
 </main>
 
 <style>
   main {
-    text-align: center;
-    background-color: #ffffff;
-    margin-top: 50px;
-    font-family: Arial;
+    height: 100%;
+    display: flex;
     user-select: none;
+    font-family: Arial;
+    text-align: center;
+    align-items: center;
+    flex-direction: column;
+    justify-content: flex-end;
+
+    /* Disable selection */
     -ms-user-select: none;
     -moz-user-select: none;
     -khtml-user-select: none;
     -webkit-user-select: none;
     -webkit-touch-callout: none;
-  }
-
-  input {
-    width: 780px;
-    border: none;
-    font-size: large;
-    text-align: center;
-  }
-
-  input:focus,
-  input:active {
-    outline: none;
   }
 </style>
