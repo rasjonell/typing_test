93b2d853fc4e5a2fb55d3e94a2e0addf4be2e118
rasjonell <info.gurgen@gmail.com>
2022-06-06 20:34:24 +0400

    Separate random text generator into it's own file

---
+++ src/utils/random_paragraph.ts
@@ -0,0 +1,25 @@
+export default async function attachRandomParagraph(parentNode: HTMLElement) {
+  const response = await fetch("http://metaphorpsum.com/sentences/10");
+  const paragraph = await response.text();
+
+  const htmlText = paragraph.split(" ").map((word, index) => {
+    const span = document.createElement("span");
+
+    span.textContent = word;
+    span.setAttribute("id", (index + 1).toString());
+    span.classList.add("panel__word");
+
+    return span;
+  });
+
+  for (const child of htmlText) {
+    const whitespace = document.createElement("span");
+
+    whitespace.textContent = " ";
+    parentNode.appendChild(child);
+
+    parentNode.append(child, whitespace);
+  }
+
+  return {paragraph, htmlText}
+}
