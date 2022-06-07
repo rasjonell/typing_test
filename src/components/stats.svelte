c2fac11b53f253fc6e0c1c9dce3e5c744084e9dc
rasjonell <info.gurgen@gmail.com>
2022-06-07 20:44:13 +0400

    Add Infographic Widgets

--- src/components/stats.svelte
+++ src/components/stats.svelte
@@ -1,63 +1,68 @@
 <script lang="ts">
+  // Core
   import { onMount } from "svelte";
 
-  import { Chart, registerables } from "chart.js";
-  Chart.register(...registerables);
+  // Helpers
+  import setupChart from "../utils/chart";
+  import { appendResults } from "../utils/chart_data";
 
   export let WPM: number;
   export let AWPM: number;
   export let accuracy: number;
 
-  let chartValues = [45];
-  let chartLabels = ["06/07 23:20"];
-  let ctx: CanvasRenderingContext2D;
   let chartCanvas: HTMLCanvasElement;
 
   onMount(() => {
-    ctx = chartCanvas.getContext("2d");
-    const chart = new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: chartLabels,
-        datasets: [
-          {
-            label: "WPM",
-            backgroundColor: "rgb(255, 99, 132)",
-            borderColor: "rgb(255, 99, 132)",
-            data: chartValues,
-          },
-          {
-            label: "AWPM",
-            backgroundColor: "rgb(99, 255, 132)",
-            borderColor: "rgb(99, 255, 132)",
-            data: [30],
-          },
-        ],
-      },
-    });
+    setupChart(chartCanvas, appendResults(WPM, AWPM));
   });
 </script>
 
 <div class="stats__container">
-  <h1>Results</h1>
-  <div>
-    <span>WPM</span>/<span>AWPM</span>
-    <span>{WPM}/{AWPM}</span> Words Per Minute
-  </div>
-  <div>
-    <span>{accuracy}%</span> Accuracy
+  <div class="stats__widgets">
+    <div>
+      <div class="ui-widget">
+        <h1 class="ui-value">{WPM}</h1>
+        <span class="ui-label">WPM</span>
+      </div>
+    </div>
+    <div>
+      <div class="ui-widget">
+        <h1 class="ui-value">{accuracy}%</h1>
+        <span class="ui-label">Accuracy</span>
+      </div>
+    </div>
+    <div>
+      <div class="ui-widget">
+        <h1 class="ui-value">{AWPM}</h1>
+        <span class="ui-label">AWPM</span>
+      </div>
+    </div>
   </div>
   <canvas bind:this={chartCanvas} />
   <div class="stats__actions">
-    <button on:click={() => location.reload()}>Retake the Test!</button>
-    <button>Share Your Results!</button>
+    <button
+      class="btn-grad"
+      on:click={() =>
+        window.open(
+          `https://twitter.com/intent/tweet?text=I can type ${AWPM} Accurate Words Per Minute. Can you beat my record?`
+        )}
+    >
+      Tweet Your Results!
+    </button>
+    <button class="btn-grad" on:click={() => location.reload()}>
+      Retake the Test!
+    </button>
   </div>
 </div>
 
 <style>
   .stats__container {
-    height: 50vh;
+    height: 75vh;
     color: white;
+    display: flex;
+    margin-bottom: 10px;
+    flex-direction: column;
+    justify-content: space-between;
   }
 
   button {
@@ -67,8 +72,74 @@
 
   .stats__actions {
     display: flex;
-    margin-top: 20px;
     align-items: center;
-    justify-content: flex-end;
+    justify-content: space-between;
+  }
+
+  .stats__widgets {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .ui-widget {
+    position: relative;
+    display: inline-block;
+    width: 160px;
+    height: 160px;
+    margin: 10px;
+    border: 15px solid #00aeef;
+    border-radius: 150px;
+    text-align: center;
+    font-family: Arial, sans-serif;
+    box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.8),
+      inset 0 3px 5px rgba(0, 0, 0, 0.25);
+    box-sizing: border-box;
+    border-bottom-color: #fff;
+  }
+
+  .ui-widget .ui-value {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 15px;
+    font-size: 40px;
+  }
+
+  .ui-label {
+    position: absolute;
+    left: 0;
+    bottom: -40px;
+    width: 100%;
+    font-size: 14px;
+    font-weight: bold;
+    text-transform: uppercase;
+    color: white;
+    font-weight: 700;
+  }
+
+  .btn-grad {
+    background-image: linear-gradient(
+      to right,
+      #aa076b 0%,
+      #61045f 51%,
+      #aa076b 100%
+    );
+    margin: 10px;
+    padding: 15px 45px;
+    text-align: center;
+    text-transform: uppercase;
+    transition: 0.5s;
+    background-size: 200% auto;
+    color: white;
+    border-radius: 10px;
+    display: block;
+    border: none;
+  }
+
+  .btn-grad:hover {
+    background-position: right center;
+    color: #fff;
+    text-decoration: none;
   }
 </style>
